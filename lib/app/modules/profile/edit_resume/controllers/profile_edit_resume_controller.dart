6637b1e31cea4ae69cc8411dd717980cd3d20354
Irfan Maulana <irfan.resimen2018@gmail.com>
2023-12-05 14:42:52 +0700

    feat: add file function

--- lib/app/modules/profile/edit_resume/controllers/profile_edit_resume_controller.dart
+++ lib/app/modules/profile/edit_resume/controllers/profile_edit_resume_controller.dart
@@ -1,23 +1,58 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProfileEditResumeController extends GetxController {
-  //TODO: Implement ProfileEditResumeController
+  final firestore = FirebaseFirestore.instance;
+  TextEditingController title = TextEditingController();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  final storage = FirebaseStorage.instance;
+
+  File? resumeFile;
+  var isFilePicked = false.obs;
+
+  void addResumeFile() async {
+    try {
+      await storage.ref('resume').putFile(resumeFile!);
+      await firestore.collection('resume').add({
+        'title': title.text,
+        'resume': resumeFile,
+      });
+      Get.snackbar('Success', 'Resume Added Successfully',
+          backgroundColor: Colors.green, colorText: Colors.white);
+    } catch (e) {
+      print(e);
+    }
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void addResume() async {
+    try {
+      await firestore.collection('resume').add({
+        'title': title.text,
+        'resume': resumeFile,
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  void pickFile() async {
+    try {
+      final result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['pdf'],
+      );
+      if (result != null) {
+        resumeFile = File(result.files.single.path!);
+        update();
+      }
+    } catch (e) {
+      print(e);
+    }
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/profile/edit_resume/views/profile_edit_resume_view.dart
+++ lib/app/modules/profile/edit_resume/views/profile_edit_resume_view.dart
@@ -13,17 +13,45 @@ class ProfileEditResumeView extends GetView<ProfileEditResumeController> {
         title: const Text('ProfileEditResumeView'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Container(
-            child: TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Name',
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Container(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.file_copy_rounded),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: 'File Name (For Display)',
+                ),
+                maxLength: 8,
               ),
-            maxLength: 8,
             ),
-          ),
-        ],
+            Obx(
+              () => (controller.isFilePicked == false)
+                  ? Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          controller.pickFile();
+                        },
+                        child: const Text('Upload Resume'),
+                      ),
+                    )
+                  : Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          controller.addResumeFile();
+                        },
+                        child: const Text('Save Resume'),
+                      ),
+                    ),
+            ),
+          ],
+        ),
       ),
     );
   }
