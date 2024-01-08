3689a8c9ce0a39bc24b76b97ee8a3a7fa61babe4
Irfan Maulana <irfan.resimen2018@gmail.com>
2024-01-08 17:18:09 +0700

    feat: add delete experience feature

--- lib/app/modules/profile/edit_experience/views/profile_edit_experience_view.dart
+++ lib/app/modules/profile/edit_experience/views/profile_edit_experience_view.dart
@@ -33,111 +33,113 @@ class ProfileEditExperienceView
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            CustomTextfield(
-              prefixIcon: Icons.work_outline,
-              hintText: 'Job Title',
-              controller: controller.title,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            CustomTextfield(
-              prefixIcon: Icons.corporate_fare_rounded,
-              hintText: 'Company/Organization',
-              controller: controller.company,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Obx(
-              () => TextFormField(
-                initialValue: controller.startDate.value,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.date_range),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  hintText: (controller.startDate.value == '')
-                      ? 'Start Date'
-                      : controller.startDate.value.replaceRange(10, 23, ''),
-                ),
-                onTap: () async {
-                  final selectedDate = await showMonthYearPicker(
-                    context: Get.context!,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2030),
-                  );
-                  if (selectedDate != null) {
-                    controller.startDate.value = selectedDate.toString();
-                    print(controller.startDate.value.toString());
-                  }
-                },
-                readOnly: true,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              CustomTextfield(
+                prefixIcon: Icons.work_outline,
+                hintText: 'Job Title',
+                controller: controller.title,
               ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Obx(
-              () => Row(
-                children: [
-                  Checkbox(
-                    value: controller.isCurrent.value,
-                    onChanged: (value) {
-                      controller.isCurrent.value = value!;
-                    },
-                  ),
-                  Text('I currently work here'),
-                ],
+              const SizedBox(
+                height: 16,
               ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Obx(() => Visibility(
-                  visible: controller.isCurrent.value == false,
-                  child: TextFormField(
-                    initialValue: controller.endDate.value,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.date_range),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      hintText: (controller.endDate.value == '')
-                          ? 'End Date'
-                          : controller.endDate.value.replaceRange(10, 23, ''),
+              CustomTextfield(
+                prefixIcon: Icons.corporate_fare_rounded,
+                hintText: 'Company/Organization',
+                controller: controller.company,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Obx(
+                () => TextFormField(
+                  initialValue: controller.startDate.value,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.date_range),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
                     ),
-                    onTap: () async {
-                      final selectedDate = await showMonthYearPicker(
-                        context: Get.context!,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(2000),
-                        lastDate: DateTime(2030),
-                      );
-                      if (selectedDate != null) {
-                        controller.endDate.value = selectedDate.toString();
-                        print(controller.endDate.value.toString());
-                      }
-                    },
-                    readOnly: true,
+                    hintText: (controller.startDate.value == '')
+                        ? 'Start Date'
+                        : controller.startDate.value.replaceRange(10, 23, ''),
                   ),
-                )),
-            const SizedBox(
-              height: 32,
-            ),
-            Obx(
-              () => CustomButton(
-                isLoading: controller.isLoading.value,
-                text: 'Save',
-                onTap: () {
-                  controller.saveExperience();
-                },
+                  onTap: () async {
+                    final selectedDate = await showMonthYearPicker(
+                      context: Get.context!,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2030),
+                    );
+                    if (selectedDate != null) {
+                      controller.startDate.value = selectedDate.toString();
+                      print(controller.startDate.value.toString());
+                    }
+                  },
+                  readOnly: true,
+                ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 16,
+              ),
+              Obx(
+                () => Row(
+                  children: [
+                    Checkbox(
+                      value: controller.isCurrent.value,
+                      onChanged: (value) {
+                        controller.isCurrent.value = value!;
+                      },
+                    ),
+                    Text('I currently work here'),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Obx(() => Visibility(
+                    visible: controller.isCurrent.value == false,
+                    child: TextFormField(
+                      initialValue: controller.endDate.value,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.date_range),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        hintText: (controller.endDate.value == '')
+                            ? 'End Date'
+                            : controller.endDate.value.replaceRange(10, 23, ''),
+                      ),
+                      onTap: () async {
+                        final selectedDate = await showMonthYearPicker(
+                          context: Get.context!,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2030),
+                        );
+                        if (selectedDate != null) {
+                          controller.endDate.value = selectedDate.toString();
+                          print(controller.endDate.value.toString());
+                        }
+                      },
+                      readOnly: true,
+                    ),
+                  )),
+              const SizedBox(
+                height: 32,
+              ),
+              Obx(
+                () => CustomButton(
+                  isLoading: controller.isLoading.value,
+                  text: 'Save',
+                  onTap: () {
+                    controller.saveExperience();
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
