00ef69c646dd5cda11db3c1fb18dd09850f38e6e
Irfan Maulana <irfan.resimen2018@gmail.com>
2024-01-08 16:14:00 +0700

    feat: fix resume view logic

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -26,6 +26,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin cloud_firestore, io.flutter.plugins.firebase.firestore.FlutterFirebaseFirestorePlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new dev.fluttercommunity.plus.device_info.DeviceInfoPlusPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin device_info_plus, dev.fluttercommunity.plus.device_info.DeviceInfoPlusPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new com.mr.flutter.plugin.filepicker.FilePickerPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin file_picker, com.mr.flutter.plugin.filepicker.FilePickerPlugin", e);
@@ -89,6 +94,11 @@ public final class GeneratedPluginRegistrant {
       flutterEngine.getPlugins().add(new io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin shared_preferences_android, io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin", e);
+    }
+    try {
+      flutterEngine.getPlugins().add(new com.syncfusion.flutter.pdfviewer.SyncfusionFlutterPdfViewerPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin syncfusion_flutter_pdfviewer, com.syncfusion.flutter.pdfviewer.SyncfusionFlutterPdfViewerPlugin", e);
     }
     try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.urllauncher.UrlLauncherPlugin());
--- lib/app/modules/profile/views/resume_view.dart
+++ lib/app/modules/profile/views/resume_view.dart
@@ -2,21 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 import 'package:getx_skeleton/app/modules/profile/controllers/profile_controller.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class ResumeView extends StatefulWidget {
+class ResumeView extends StatelessWidget {
   ResumeView({this.pdfUrl});
   String? pdfUrl;
 
   @override
-  State<ResumeView> createState() => _ResumeViewState();
-}
-
-class _ResumeViewState extends State<ResumeView> {
-  @override
   Widget build(BuildContext context) {
-    final c = Get.find<ProfileController>();
-
-   InAppWebViewController _webViewController;
 
     return Scaffold(
       appBar: AppBar(
@@ -29,11 +22,10 @@ class _ResumeViewState extends State<ResumeView> {
             },
             icon: Icon(Icons.close, color: Colors.black),
           )),
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(url: Uri.parse(widget.pdfUrl!)),
-        onWebViewCreated: (InAppWebViewController controller) {
-          _webViewController = controller;
-        },
+      body: Container(
+        child: SfPdfViewer.network(
+          pdfUrl!,
+        ),
       ),
     );
   }
